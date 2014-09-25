b672c41e94509133d5da3301ddfcaa59829112a1
Maxx Boehme <maxxboehme@gmail.com>
2014-09-25 15:16:01 -0700

    Edited Makefile and file tree

---
+++ source/main.cpp
@@ -0,0 +1,24 @@
+// -------
+// defines
+// -------
+
+#ifdef ONLINE_JUDGE
+    #define NDEBUG
+#endif
+
+// --------
+// includes
+// --------
+
+#include <iostream> // cin, cout
+
+#include "Collatz.h"
+
+// ----
+// main
+// ----
+
+int main () {
+    collatz_solve(std::cin, std::cout);
+    return 0;
+}
